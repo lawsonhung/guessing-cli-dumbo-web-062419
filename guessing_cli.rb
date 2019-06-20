2eed6a69caf7defb2734f7eb39ced4e881ef86c7
github username <github email address>
2019-06-20 01:54:56 -0400

    Done with error, works with Ruby command, ERROR with learn command

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,8 +1,16 @@
 # Code your solution here!
 
 # Prompts the user to guess a number between 1 and 6.
+
+# ?????????????????????? Works with ruby command
+# Error with learn command:
+# Errno::ENOENT:
+# No such file or directory @ rb_sysopen - --format
+
 def run_guessing_game
     puts "Guess a number between 1 and 6. Type 'exit' to quit."
+
+    # Error is possibly with gets.chomp method
     user_input = gets.chomp
     correct_number = rand(1..6)
 
