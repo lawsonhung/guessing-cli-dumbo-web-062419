3d385ebfba9ef6df86f675938edf3b7de1f0a80c
github username <github email address>
2019-06-20 01:52:11 -0400

    Done with error, works with Ruby command, ERROR with learn command

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,29 @@
 # Code your solution here!
+
+# Prompts the user to guess a number between 1 and 6.
+def run_guessing_game
+    puts "Guess a number between 1 and 6. Type 'exit' to quit."
+    user_input = gets.chomp
+    correct_number = rand(1..6)
+
+    # puts "Guess: #{user_input}, Correct answer: #{correct_number}"
+
+    while user_input != "exit"
+        if user_input.to_i == correct_number
+            puts "You guessed the correct number!"
+            break
+        else
+            puts "The computer guessed #{correct_number}"
+            # break
+        end
+        user_input = gets.chomp
+    end
+    if user_input == "exit"
+        puts "Goodbye!"
+    else
+        puts "Goodbye!"
+    end
+
+end
+
+run_guessing_game
